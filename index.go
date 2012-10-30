f1f099be147ea77e4704eca3ca95329f122bc9b5
Jose Carlos Nieto <xiam@menteslibres.org>
2012-10-30 14:15:56 -0600

    A Go utility for creating packages.json.

---
+++ index.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"github.com/gosexy/sugar"
+	"github.com/gosexy/yaml"
+	"encoding/json"
+	"os"
+	"fmt"
+)
+
+const PS = string(os.PathSeparator)
+
+func getPlugins() []string {
+	plugins := []string{}
+
+	dh, err := os.Open(".")
+	if err != nil {
+		panic(err)
+	}
+
+	files, err := dh.Readdir(-1)
+	if err != nil {
+		panic(err)
+	}
+
+	var pkgfile string
+
+	for _, file := range files {
+		if file.IsDir() == true {
+			name := file.Name()
+			pkgfile = name + PS + "package.yaml"
+			_, err = os.Stat(pkgfile)
+			if err == nil {
+				plugins = append(plugins, name)
+			}
+		}
+	}
+
+	return plugins
+}
+
+func main() {
+	var response = []sugar.Tuple{}
+
+	names := getPlugins()
+
+	for _, name := range names {
+		file := name + PS + "package.yaml"
+		pkg, err := yaml.Open(file)
+
+		if err != nil {
+			panic(err)
+		}
+
+		item := sugar.Tuple {}
+
+		keys := []string {
+			"name",
+			"stable",
+			"latest",
+			"license",
+			"url",
+			"author",
+			"copyright",
+			"packages",
+		}
+
+		for _, key := range keys {
+			item[key] = pkg.Get(key)
+		}
+
+		response = append(response, item)
+
+	}
+
+	data, _ := json.Marshal(response)
+
+	fmt.Printf("%s", string(data))
+}
